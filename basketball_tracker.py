a5ac4c9de520fde9f6f3fd8e0b7d1565eb7be1cb
isaacgege17 <isaacgege17@gmail.com>
2026-07-04 13:06:58 +0100

    Day 2 - Variables and User Input

--- basketball_tracker.py
+++ basketball_tracker.py
@@ -1,10 +1,13 @@
 print("BASKETBALL TRACKER")
 player_name = input("Enter player name: ")
-points_scored = input("Enter points scored: ")
-rebounds = input("Enter rebounds: ")
-assists = input("Enter assists: ")
+points_scored = int(input("Enter points scored: "))
+rebounds = int(input("Enter rebounds: "))
+assists = int(input("Enter assists: "))
+total = points_scored + rebounds + assists
 print("Player Name: ", player_name)
 print("Points Scored: ", points_scored)
 print("Rebounds: ", rebounds)
 print("Assists: ", assists)
+print()
+print("Total Stats: ", total)
 
