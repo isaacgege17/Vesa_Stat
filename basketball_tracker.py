88c87f7e9e5247351a53dfd368755a6d12dc37cd
isaacgege17 <isaacgege17@gmail.com>
2026-07-05 13:06:27 +0100

    Day 4 - Loops

--- basketball_tracker.py
+++ basketball_tracker.py
@@ -1,21 +1,23 @@
 print("BASKETBALL TRACKER")
-player_name = input("Enter player name: ")
-points_scored = int(input("Enter points scored: "))
-rebounds = int(input("Enter rebounds: "))
-assists = int(input("Enter assists: "))
-total = points_scored + rebounds + assists
-print("Player Name: ", player_name.upper())
-print("Points Scored: ", points_scored)
-print("Rebounds: ", rebounds)
-print("Assists: ", assists)
-print()
-print("Total Stats: ", total)
-print()
-if points_scored >= 30:
-    print("MVP Performance! ")
-elif points_scored >= 20:
-    print("Great Performance! ")
-else:
-    print("Keep working hard! ")
+for i in range(3):
+    player_name = input("Enter player name: ")
+    points_scored = int(input("Enter points scored: "))
+    rebounds = int(input("Enter rebounds: "))
+    assists = int(input("Enter assists: "))
+    total = points_scored + rebounds + assists
+    print("Player Name: ", player_name.upper())
+    print("Points Scored: ", points_scored)
+    print("Rebounds: ", rebounds)
+    print("Assists: ", assists)
+    print()
+    print("Total Stats: ", total)
+    print()
+    if points_scored >= 30:
+        print("MVP Performance! ")
+    elif points_scored >= 20:
+        print("Great Performance! ")
+    else:
+        print("Keep working hard! ")
+
 
 
